6a45137597c0e862da3c7faca9d4a0f8e8754f53
Mohammad Mohammadi <68048123+Sarbazg180@users.noreply.github.com>
2021-04-12 02:46:21 +0430

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,12 +1,39 @@
+clear
+red='\u001b[31;1m'
+nc='\033[0m'
+yellow='\033[1;33m'
+blue='\e[1;35m'
+echo -e """${red}  _    _       _            _             
+ | |  | |     | |          | |            
+ | |  | |_ __ | | ___   ___| | _____ _ __ 
+ | |  | | '_ \| |/ _ \ / __| |/ / _ \ '__|
+ | |__| | | | | | (_) | (__|   <  __/ |   
+  \____/|_| |_|_|\___/ \___|_|\_\___|_|
+  """
+echo -e "${red}#############################################${nc}"
+echo -e "${red}programmer: sarbazg180"
+echo -e "${red}credit: charchit"
+echo -e "${red}#############################################${nc}"
 
-termux-setup-storage
-
+termux-setup-storage -y > /dev/null
 cp 'config/game_patch_1.3.0.14924.pak' '/storage/emulated/0/android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/saved/Paks'
 
+sleep .8
+
+echo -e "${yellow}
+[!] start patching...${nc}"
+
+sleep .8
+
+echo -e "${yellow}
+[!] successfully unlocked."
+
+sleep .8
+echo -e "${blue}
+[!] running the game..."
+sleep .8
+
+am start --user 0 -n com.tencent.ig/com.epicgames.ue4.SplashActivity > /dev/null
+
 chmod +x pubg
-
 cp pubg $HOME/../usr/bin
-
-clear
-
-echo 'run the game'
