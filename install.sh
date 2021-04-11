a1f92a8e7d2b121a7c2186b20306cf8c0e504db4
Mohammad Mohammadi <68048123+Sarbazg180@users.noreply.github.com>
2021-04-12 00:20:34 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
 
+termux-setup-storage
+
+cp 'config/game_patch_1.3.0.14924.pak' '/storage/emulated/0/android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/saved/Paks'
+
+chmod +x pubg
+
+cp pubg $HOME/../usr/bin
+
+clear
+
+echo 'run the game'
