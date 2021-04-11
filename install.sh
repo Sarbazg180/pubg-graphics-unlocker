f62a75cee94b4547868d610ea69b0bfd9c19fa71
Mohammad Mohammadi <68048123+Sarbazg180@users.noreply.github.com>
2021-04-11 23:57:52 +0430

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+termux-setup-storage
+cp 'config/game_patch_1.3.0.14924.pak' '/storage/emulated/0/android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/saved/Paks'
+clear
+echo 'run the game'
